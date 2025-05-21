15c9d3d88a70ebc92f092439d2af83ff524c3003
LipeFB <felipeboth12@gmail.com>
2025-05-20 21:00:46 -0300

    Introducao a funcoes no JS

---
+++ funcoes/desafio_com_funcoes.js
@@ -0,0 +1,15 @@
+// Desafio:
+// Criar uma funcao que receba dois numeros/parametros, imprima na tela e some os dois, e uma que subtraia os dois
+
+function somar(num1, num2) {
+  console.log(num1 + num2);
+}
+
+
+function subtrair(num1, num2) {
+  console.log(num1 - num2);
+}
+
+
+somar(20, 40);
+subtrair(100, 80);
---
+++ funcoes/sem_funcao.js
@@ -0,0 +1,21 @@
+//   Sintaxe ou Anatomia da Funcao 
+// function nomeDaFuncao (parametros) {
+//   corpo da funcao
+//   passos a serem executados
+//  }
+
+const d1 = 20;
+const m1 = 5;
+const a1 = 2025;
+
+console.log(`Dia: ${d1}`);
+console.log(`Mes: ${m1}`);
+console.log(`Ano: ${a1}`);
+
+const d2 = 21;
+const m2 = 6;
+const a2 = 2025;
+
+console.log(`Dia: ${d2}`);
+console.log(`Mes: ${m2}`);
+console.log(`Ano: ${a2}`);
